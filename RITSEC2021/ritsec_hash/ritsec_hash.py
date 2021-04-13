4a688247c75950bfe2978b92f123757968f53554
teo437 <82166608+teo437@users.noreply.github.com>
2021-04-13 14:56:53 +0200

    renamed and cleaned the script

--- RITSEC2021/ritsec_hash/ritsec_hash.py
+++ RITSEC2021/ritsec_hash/ritsec_hash.py
@@ -10,47 +10,61 @@ def round(old_digest, x, r):
 
     new_digest = bytearray(6)
     
-    # ops are numbered by order of appearance
-    
     # computing A
     new_digest[0] = (((C ^ E) & F) + B + (D << 2) + x + r) & 0xFF
 
-    #computing B
+    # computing B
     new_digest[1] = A
     
-    #computing C
+    # computing C
     new_digest[2] = (D << 2) & 0xFF
     
-    #computing D
+    # computing D
     new_digest[3] = (B >> 5) & 0xFF
     
-    #computing E
+    # computing E
     new_digest[4] = (A + F) & 0xFF
     
-    #computing F
+    # computing F
     new_digest[5] = D
     
     return new_digest
     
     
 def h_function(digest, x):
-
-    tmp_digest = digest
     
     for round_number in range(13):
-        tmp_digest = round(tmp_digest, x, round_number)
-    
+        digest = round(digest, x, round_number)
     return tmp_digest
     
+    
 def hash(bytes_to_hash):
+    
     H = bytearray(b"RITSEC")
     for x in bytes_to_hash:
         H = h_function(H, x)
     return H
 
+
+# validating the hash's implementation
+if hash(b"RITSEC_CTF_2021") == bytes.fromhex("3ba50807aa02"):
+    print("hash function working")
+
+# dictionary attack
+with open("/usr/share/wordlists/rockyou.txt", "rb") as wordlist:
+    for word in wordlist:
+        res = hash(word.rstrip(b'\n'))
+        if res == bytes.fromhex("435818055906"):
+            print("FLAG : RS{" + word + "}")
+
+            
 """
+# bruteforce attack
+
 charset = range(0x20, 0x7f)
 length = 6
+
+
 def generate_string(string, depth):
     if depth == 0:
         string[length - 1] = 109 # 'm'
@@ -62,11 +76,6 @@ def generate_string(string, depth):
             string[length - depth - 1] = c
             generate_string(string, depth-1)
 
+
 generate_string(bytearray(length), length-1)
 """
-
-with open("/usr/share/wordlists/rockyou.txt", "rb") as f:
-    for line in f:
-        res = hash(line[:-1])
-        if res == b'\x43\x58\x18\x05\x59\x06':
-            print(line)
